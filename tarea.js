c30159216c88b3fd6042314bf6a198e26b30e159
ivan-burgio <ivan.24.burgio@gmail.com>
2023-05-02 00:19:56 -0300

    fin js

--- tarea.js
+++ tarea.js
@@ -60,20 +60,59 @@ function boolRandom() {
     return numRandom() > 50;
 }
 
+// Entrego las 3 evidencias
+function evidencias(completo) {
+    return completo[0] && completo[1] && completo[2];
+}
+
 // Evalua Competencias transversales
 function completoCT(completo) {
     let resultado = '';
     if(completo) {
-        resultado = 'Aprobado';
+        resultado = '100 puntos - Aprobado';
     } else {
         resultado = 'Faltan lecciones';
     }
     return resultado;
 }
 
-// Evalua la parte Tecnica
+// Evalua Tecnica
 function completoTecnico(completo) {
+    let resultado = '';
+    let porcentaje = Math.trunc(completo.puntajes[0]/3 + completo.puntajes[1]/5 + completo.puntajes[2]/2);
+    if(porcentaje >= 60 && evidencias(completo.bools)) {
+        resultado = 'Aprobado';
+    } else if(porcentaje >= 60) {
+        resultado = 'Faltan evidencias';
+    } else if(evidencias(completo.bools)) {
+        resultado = 'Tienes menos de 60 puntos'
+    } else {
+        resultado = 'Tienes menos de 60 puntos y faltan evidencias'
+    }
+    return porcentaje + ' puntos - ' + resultado;
+}
 
+// Evalua Ingles
+function ingles(completo) {
+    let resultado = '';
+    if(completo >= 50) {
+        resultado = completo + ' puntos - Aprobado';
+    } else {
+        resultado = completo + ' puntos - Desaprobado';
+    }
+    return resultado;
+}
+
+// Conclusion
+function final(completo) {
+    let resultado = '';
+    let porcentaje = Math.trunc(completo.puntajes[0]/3 + completo.puntajes[1]/5 + completo.puntajes[2]/2);
+    if((completo.bools[3]) && porcentaje >= 60 && evidencias(completo.bools) && completo.puntajes[3] >= 50) {
+        resultado = 'Aprobado';
+    } else {
+        resultado = 'Desaprobado';
+    }
+    return resultado;
 }
 
 // Muestra en pantalla el estado del estudiante
@@ -81,9 +120,11 @@ function detallesStatus(datos) {
     const mensaje = document.querySelector('.contenido');
     const resultado = document.createElement('P');
     resultado.innerHTML = `
-        Estimado ${datos.estudiante.nombre}, sus calificaciones son:
-        Competencias Transversales: ${completoCT(datos.bools[3])}
-        Introducción a la Programación: $
+        Estimado <span>${datos.estudiante.nombre} ${datos.estudiante.apellido}</span>, sus calificaciones son:<br><br>
+        <span>Competencias Transversales:</span> ${completoCT(datos.bools[3])}<br>
+        <span>Tecnico:</span> ${completoTecnico(datos)}<br>
+        <span>Ingles:</span> ${ingles(datos.puntajes[3])}<br><br>
+        <span>Conclusión:</span> ${final(datos)}
     `;
     mensaje.appendChild(resultado);
 }
